ed34b90d7b7264027ed2d4bbe12233abe6f8e2e2
zionmedia <yes@zionmedia.com>
2019-01-08 10:50:11 +0800

    世界观书籍整理

--- index.py
+++ index.py
@@ -2,10 +2,9 @@
 from __future__ import print_function
 import os.path, time
 
-INDEX_TEMPLATE = r"""
-
-<html>
+INDEX_TEMPLATE = r"""<html>
 <head>
+<meta charset="UTF-8">
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <title>zionmedia fs: ${header}</title>
